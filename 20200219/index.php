2900d765e829b3acfe125bb32f8e21c269a554ec
julia2003-pixel <juliaregina03@outlook.com>
2020-02-19 17:47:10 -0300

    exs

---
+++ 20200219/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Pagina Inicial com Bootstrap</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+    
+</head>
+<body>
+    <div class="row">
+        <h1> Pagina com links das minhas atividades </h1>
+    </div class>
+</body>
+</html>
