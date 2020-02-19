d9cb2abb8db4c6fdcddebc09247d939b24d2a78a
julia2003-pixel <juliaregina03@outlook.com>
2020-02-19 16:30:17 -0300

    exercicios

---
+++ 20200219/barbearia/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Pagina Inicial com Bootstrap</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+    
+</head>
+<body>
+    <div class="row">
+        <p>
+             bem vindo a barbearia.
+         </p>  
+         <div class="col-xs-6col-sm-4">
+             <div class ="panel panel-primary">
+                 <div class="panel-heading">
+                     <h3> class="panel-title">Titulo do Painel</h3>
+                 </div>
+                 <div class =" panel-body">
+                        agende seu horário.
+                 </div>
+             </div>
+         </div>  
+    </div>
+</div>
+    <script src ="js/jquery-3.3.1.min.js"></script>
+    <script src ="js/bootstrap.min.js"></script>
+</body>
+</html>
